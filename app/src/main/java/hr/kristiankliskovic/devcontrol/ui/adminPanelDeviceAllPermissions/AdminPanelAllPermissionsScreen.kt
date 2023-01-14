b598f3399d7a1d035945d5bfffd5943642912d46
KristianK1 <kikihd11@gmail.com>
2023-01-14 11:36:18 +0100

    seeAllPerms - okay design

--- app/src/main/java/hr/kristiankliskovic/devcontrol/ui/adminPanelDeviceAllPermissions/AdminPanelAllPermissionsScreen.kt
+++ app/src/main/java/hr/kristiankliskovic/devcontrol/ui/adminPanelDeviceAllPermissions/AdminPanelAllPermissionsScreen.kt
@@ -1,11 +1,8 @@
 package hr.kristiankliskovic.devcontrol.ui.adminPanelDeviceAllPermissions
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -15,13 +12,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import hr.kristiankliskovic.devcontrol.R
+import hr.kristiankliskovic.devcontrol.ui.theme.Shapes
 
 @Composable
 fun SeeAllPermissionsRoute(
@@ -64,61 +64,124 @@ fun SeeAllPermissionsScreen(
         Text(
             text = stringResource(id = R.string.seeAllPermissions_devicePermissions_title)
         )
-        for (devRight in viewState.permissions) {
-            UserPermissionLabel(
-                username = devRight.username,
-                isWrite = !devRight.readOnly,
-                onClick = {
-
-                }
+        if (viewState.permissions.isEmpty()) {
+            Text(
+                text = stringResource(id = R.string.seeAllPermissionsScreen_noPermission_device),
+                fontSize = 15.sp
             )
+        } else {
+            for (permission in viewState.permissions) {
+                UserPermissionLabel(
+                    username = permission.username,
+                    isWrite = !permission.readOnly,
+                    onClick = {
+                        deleteDevicePermission(viewState.deviceId, permission.userId)
+                    }
+                )
+            }
         }
-//        Text(
-//            text = stringResource(id = R.string.seeAllPermissions_groupPermissions_title)
-//        )
-//        for (group in viewState.groupPermissions) {
-//            Column(
-//                modifier = Modifier
-//                    .border(
-//                        color = colorResource(id = R.color.seeAllPermissionScreen_rights_borders),
-//                        width = dimensionResource(id = R.dimen.seeAllPermissionScreen_rights_borders)
-//                    )
-//            ) {
-//                for (groupRight in group.permissions)
-//                    UserPermissionLabel(
-//                        username = groupRight.username,
-//                        isWrite = !groupRight.readOnly,
-//                        onClick = {
-//
-//                        }
-//                    )
-//                Text(
-//                    text = stringResource(id = R.string.seeAllPermissions_fieldPermissions_title)
-//                )
-//                for (field in group.fields) {
-//                    Column(
-//                        modifier = Modifier
-//                            .border(
-//                                color = colorResource(id = R.color.seeAllPermissionScreen_rights_borders),
-//                                width = dimensionResource(id = R.dimen.seeAllPermissionScreen_rights_borders)
-//                            )
-//                    ) {
-//                        for (fieldRight in field.permissions)
-//                            UserPermissionLabel(
-//                                username = fieldRight.username,
-//                                isWrite = !fieldRight.readOnly,
-//                                onClick = {
-//
-//                                }
-//                            )
-//                    }
-//                }
-//            }
-//        }
+
+        GroupPermissions(
+            groupViewStates = viewState.groupPermissions,
+            deleteGroupPermission = { groupId: Int, userId: Int ->
+                deleteGroupPermission(viewState.deviceId, groupId, userId)
+            },
+            deleteFieldPermission = { groupId: Int, fieldId: Int, userId: Int ->
+                deleteFieldPermission(viewState.deviceId, groupId, fieldId, userId)
+            }
+        )
         ComplexGroupPermissions(
             complexGroupViewStates = viewState.complexGroupPermissions,
             deleteComplexGroupPermission = { complexGroupId: Int, userId: Int ->
                 deleteComplexGroupPermission(viewState.deviceId, complexGroupId, userId)
+            }
+        )
+    }
+}
+
+
+@Composable
+fun GroupPermissions(
+    groupViewStates: List<UserPermissionToGroupViewState>,
+    deleteGroupPermission: (Int, Int) -> Unit,
+    deleteFieldPermission: (Int, Int, Int) -> Unit,
+) {
+    if (groupViewStates.isNotEmpty()) {
+        Column(
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_for_border))
+                .clip(Shapes.small)
+                .border(
+                    color = colorResource(id = R.color.seeAllPermissionScreen_rights_borders),
+                    width = dimensionResource(id = R.dimen.seeAllPermissionScreen_rights_borders)
+                )
+                .background(
+                    color = colorResource(id = R.color.seeAllPermissionsScreen_groups_background)
+                )
+                .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_between_boxes))
+        ) {
+            Text(
+                text = stringResource(id = R.string.seeAllPermissions_groupPermissions_title),
+                fontSize = 25.sp,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            for (groupViewState in groupViewStates) {
+                GroupPermission(
+                    groupViewState = groupViewState,
+                    deleteGroupPermission = deleteGroupPermission,
+                    deleteFieldPermission = deleteFieldPermission,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun GroupPermission(
+    groupViewState: UserPermissionToGroupViewState,
+    deleteGroupPermission: (Int, Int) -> Unit,
+    deleteFieldPermission: (Int, Int, Int) -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_for_border))
+            .clip(Shapes.small)
+            .border(
+                color = colorResource(id = R.color.seeAllPermissionScreen_rights_borders),
+                width = dimensionResource(id = R.dimen.seeAllPermissionScreen_rights_borders)
+            )
+            .background(
+                color = colorResource(id = R.color.seeAllPermissionsScreen_group_background)
+            )
+            .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_between_boxes))
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "${stringResource(id = R.string.seeAllPermissionsScreen_fieldPermission_groupName_title)}\n${groupViewState.groupName}",
+            fontSize = 25.sp
+        )
+        if (groupViewState.permissions.isEmpty()) {
+            Text(
+                text = stringResource(id = R.string.seeAllPermissionsScreen_noPermission_group),
+                fontSize = 15.sp
+            )
+        } else {
+            for (permission in groupViewState.permissions) {
+                UserPermissionLabel(
+                    username = permission.username,
+                    isWrite = !permission.readOnly,
+                    onClick = {
+                        deleteGroupPermission(groupViewState.groupId, permission.userId)
+                    }
+                )
+            }
+        }
+        FieldPermissions(
+            fieldViewStates = groupViewState.fields,
+            deleteFieldPermission = { fieldId: Int, userId: Int ->
+                deleteFieldPermission(groupViewState.groupId, fieldId, userId)
             }
         )
     }
@@ -130,17 +193,34 @@ fun ComplexGroupPermissions(
     deleteComplexGroupPermission: (Int, Int) -> Unit,
 ) {
     if (complexGroupViewStates.isNotEmpty()) {
-        Text(
-            text = stringResource(id = R.string.seeAllPermissions_complexGroupPermissions_title),
-            fontSize = 20.sp,
-        )
-        for (complexGroupViewState in complexGroupViewStates) {
-            ComplexGroupPermission(
-                complexGroupViewState = complexGroupViewState,
-                deleteComplexGroupPermission = { complexGroupId: Int, userId: Int ->
-                    deleteComplexGroupPermission(complexGroupId, userId)
-                }
+        Column(
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_for_border))
+                .clip(Shapes.small)
+                .border(
+                    color = colorResource(id = R.color.seeAllPermissionScreen_rights_borders),
+                    width = dimensionResource(id = R.dimen.seeAllPermissionScreen_rights_borders)
+                )
+                .background(
+                    color = colorResource(id = R.color.seeAllPermissionsScreen_groups_background)
+                )
+                .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_between_boxes))
+        ) {
+            Text(
+                text = stringResource(id = R.string.seeAllPermissions_complexGroupPermissions_title),
+                fontSize = 25.sp,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
             )
+            for (complexGroupViewState in complexGroupViewStates) {
+                ComplexGroupPermission(
+                    complexGroupViewState = complexGroupViewState,
+                    deleteComplexGroupPermission = { complexGroupId: Int, userId: Int ->
+                        deleteComplexGroupPermission(complexGroupId, userId)
+                    }
+                )
+            }
         }
     }
 }
@@ -153,20 +233,24 @@ fun ComplexGroupPermission(
     Column(
         modifier = Modifier
             .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_for_border))
+            .clip(Shapes.small)
             .border(
                 color = colorResource(id = R.color.seeAllPermissionScreen_rights_borders),
                 width = dimensionResource(id = R.dimen.seeAllPermissionScreen_rights_borders)
+            )
+            .background(
+                color = colorResource(id = R.color.seeAllPermissionsScreen_group_background)
             )
             .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_between_boxes))
             .fillMaxWidth()
     ) {
         Text(
-            text = "${stringResource(id = R.string.seeAllPermissionsScreen_fieldPermission_complexGroupName_title)} ${complexGroupViewState.complexGroupName}",
+            text = "${stringResource(id = R.string.seeAllPermissionsScreen_fieldPermission_complexGroupName_title)}\n${complexGroupViewState.complexGroupName}",
             fontSize = 25.sp
         )
         if (complexGroupViewState.permissions.isEmpty()) {
             Text(
-                text = stringResource(id = R.string.seeAllPermissionsScreen_noPermission_field),
+                text = stringResource(id = R.string.seeAllPermissionsScreen_noPermission_complexGroup),
                 fontSize = 15.sp
             )
         } else {
@@ -186,27 +270,80 @@ fun ComplexGroupPermission(
 
 @Composable
 fun FieldPermissions(
+    fieldViewStates: List<UserPermissionToFieldViewState>,
+    deleteFieldPermission: (Int, Int) -> Unit,
+) {
+    if (fieldViewStates.isNotEmpty()) {
+        Column(
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_for_border))
+                .clip(Shapes.small)
+                .border(
+                    color = colorResource(id = R.color.seeAllPermissionScreen_rights_borders),
+                    width = dimensionResource(id = R.dimen.seeAllPermissionScreen_rights_borders)
+                )
+                .background(
+                    color = colorResource(id = R.color.seeAllPermissionsScreen_fields_background)
+                )
+                .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_between_boxes))
+        ) {
+            Text(
+                text = stringResource(id = R.string.seeAllPermissions_fieldPermissions_title),
+                fontSize = 25.sp,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            for (fieldViewState in fieldViewStates) {
+                FieldPermission(
+                    fieldViewState = fieldViewState,
+                    deleteFieldPermission = { fieldId: Int, userId: Int ->
+                        deleteFieldPermission(fieldId, userId)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun FieldPermission(
     fieldViewState: UserPermissionToFieldViewState,
     deleteFieldPermission: (Int, Int) -> Unit,
 ) {
-    Text(
-        text = "${stringResource(id = R.string.seeAllPermissionsScreen_fieldPermission_fieldName_title)} ${fieldViewState.fieldName}",
-        fontSize = 20.sp
-    )
-    if (fieldViewState.permissions.isEmpty()) {
-        Text(
-            text = stringResource(id = R.string.seeAllPermissionsScreen_noPermission_field),
-            fontSize = 15.sp
-        )
-    } else {
-        for (permission in fieldViewState.permissions) {
-            UserPermissionLabel(
-                username = permission.username,
-                isWrite = !permission.readOnly,
-                onClick = {
-                    deleteFieldPermission(fieldViewState.fieldId, permission.userId)
-                }
+    Column(
+        modifier = Modifier
+            .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_for_border))
+            .clip(Shapes.small)
+            .border(
+                color = colorResource(id = R.color.seeAllPermissionScreen_rights_borders),
+                width = dimensionResource(id = R.dimen.seeAllPermissionScreen_rights_borders)
             )
+            .background(
+                color = colorResource(id = R.color.seeAllPermissionsScreen_field_background)
+            )
+            .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_between_boxes))
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "${stringResource(id = R.string.seeAllPermissionsScreen_fieldPermission_fieldName_title)} ${fieldViewState.fieldName} (${fieldViewState.fieldType})",
+            fontSize = 20.sp
+        )
+        if (fieldViewState.permissions.isEmpty()) {
+            Text(
+                text = stringResource(id = R.string.seeAllPermissionsScreen_noPermission_field),
+                fontSize = 15.sp
+            )
+        } else {
+            for (permission in fieldViewState.permissions) {
+                UserPermissionLabel(
+                    username = permission.username,
+                    isWrite = !permission.readOnly,
+                    onClick = {
+                        deleteFieldPermission(fieldViewState.fieldId, permission.userId)
+                    }
+                )
+            }
         }
     }
 }
@@ -220,13 +357,19 @@ fun UserPermissionLabel(
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_padding_between_userPermissions))
+            .clip(Shapes.small)
+            .border(
+                color = colorResource(id = R.color.seeAllPermissionScreen_rights_borders),
+                width = dimensionResource(id = R.dimen.seeAllPermissionScreen_userPermission_border)
+            )
             .padding(dimensionResource(id = R.dimen.seeAllPermissionScreen_userPermissionComponent_padding)),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
             text = "${stringResource(id = R.string.seeAllPermissionsScreen_permission_username_title)} " +
-                    "$username " +
+                    "$username\n" +
                     "${stringResource(id = R.string.seeAllPermissionsScreen_permission_permissionType_title)} " +
                     if (isWrite) stringResource(id = R.string.seeAllPermissions_write_permission_label) else stringResource(
                         id = R.string.seeAllPermissions_read_permission_label),
@@ -236,6 +379,7 @@ fun UserPermissionLabel(
             imageVector = Icons.Filled.Delete,
             contentDescription = null,
             modifier = Modifier
+                .fillMaxSize()
                 .clickable {
                     onClick()
                 }
