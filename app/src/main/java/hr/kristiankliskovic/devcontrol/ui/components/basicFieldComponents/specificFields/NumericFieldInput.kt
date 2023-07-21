7fc8df183fd07f4722d75d2cd52a46505f45446d
Kristian Klišković <kikihd11@gmail.com>
2023-07-21 23:53:03 +0200

    changed numeric field to buttons + dialog slider

--- app/src/main/java/hr/kristiankliskovic/devcontrol/ui/components/basicFieldComponents/specificFields/NumericFieldInput.kt
+++ app/src/main/java/hr/kristiankliskovic/devcontrol/ui/components/basicFieldComponents/specificFields/NumericFieldInput.kt
@@ -1,26 +1,37 @@
 package hr.kristiankliskovic.devcontrol.ui.components.basicFieldComponents.specificFields
 
+import android.util.FloatMath
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Slider
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
 import hr.kristiankliskovic.devcontrol.ui.theme.Shapes
 import kotlin.math.round
 import hr.kristiankliskovic.devcontrol.R
 import hr.kristiankliskovic.devcontrol.ui.components.basicFieldComponents.specificFields.common.FieldTitle
+import hr.kristiankliskovic.devcontrol.ui.components.otherComponents.OutlineTextWrapper
+import kotlin.math.roundToInt
 
 data class NumericFieldInputViewState(
     val fieldId: Int,
@@ -37,10 +48,6 @@ fun NumericFieldInput(
     modifier: Modifier = Modifier,
     emitValue: (Float) -> Unit,
 ) {
-    val nstepsSkip = ((item.maxValue - item.minValue) / item.valueStep / 10).toInt()
-    val multipleStepsValue = nstepsSkip * item.valueStep
-    val multipleStepsValueText = "%.2f".format(multipleStepsValue)
-
     Column(
         modifier = modifier
             .border(
@@ -75,15 +82,6 @@ fun NumericFieldInput(
                 .fillMaxWidth()
         ) {
             ChangeValueButton(
-                text = "-$multipleStepsValueText",
-                onClick = {
-                    emitValue(item.currentValue + it)
-                },
-                valueDiff = -1 * multipleStepsValue,
-                modifier = Modifier
-                    .weight(1f, true)
-            )
-            ChangeValueButton(
                 text = "-${item.valueStep}",
                 onClick = {
                     emitValue(item.currentValue + it)
@@ -104,16 +102,15 @@ fun NumericFieldInput(
                 modifier = Modifier
                     .weight(1f, true)
             )
-            ChangeValueButton(
-                text = "+$multipleStepsValueText",
-                onClick = {
-                    emitValue(item.currentValue + it)
-                },
-                valueDiff = multipleStepsValue,
-                modifier = Modifier
-                    .weight(1f, true)
+            DecimalInputDialog(
+                minValue = item.minValue,
+                maxValue = item.maxValue,
+                stepValue = item.valueStep,
+                startValue = item.currentValue,
+                emitValue = {
+                    emitValue(it)
+                }
             )
-
         }
     }
 }
@@ -153,6 +150,8 @@ fun FieldValues(
         modifier = modifier
             .padding(dimensionResource(id = R.dimen.numericField_middleColumn_padding))
             .width(IntrinsicSize.Max)
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
     ) {
         Text(
             text = currentValue,
@@ -162,24 +161,123 @@ fun FieldValues(
     }
 }
 
+//@Composable
+//fun LocalValue(
+//    localValue: String,
+//    modifier: Modifier = Modifier
+//        .fillMaxSize(),
+//) {
+//    Text(
+//        text = stringResource(id = R.string.numericFieldInput_local_value),
+//        textAlign = TextAlign.Center,
+//        fontSize = 8.sp,
+//        modifier = Modifier.fillMaxWidth()
+//    )
+//    Text(
+//        text = localValue,
+//        textAlign = TextAlign.Center,
+//        fontSize = 18.sp,
+//        modifier = Modifier.fillMaxWidth()
+//    )
+//}
+
 @Composable
-fun LocalValue(
-    localValue: String,
-    modifier: Modifier = Modifier
-        .fillMaxSize(),
+fun DecimalInputDialog(
+    minValue: Float,
+    maxValue: Float,
+    stepValue: Float,
+    startValue: Float,
+    emitValue: (Float) -> Unit,
 ) {
-    Text(
-        text = stringResource(id = R.string.numericFieldInput_local_value),
-        textAlign = TextAlign.Center,
-        fontSize = 8.sp,
-        modifier = Modifier.fillMaxWidth()
-    )
-    Text(
-        text = localValue,
-        textAlign = TextAlign.Center,
-        fontSize = 18.sp,
-        modifier = Modifier.fillMaxWidth()
-    )
+    var dialogOpen by remember {
+        mutableStateOf(false)
+    }
+    if (dialogOpen) {
+        Log.i("numericSlider_onValue_minmax", "$minValue $maxValue $stepValue ${((maxValue - minValue)/stepValue).toInt()-1}")
+        AlertDialog(
+            onDismissRequest = {
+                dialogOpen = false
+            },
+            buttons = {
+                Column(
+                    modifier = Modifier
+                        .padding(dimensionResource(id = R.dimen.fieldComponent_dialog_padding)),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    var value by remember { mutableStateOf(startValue) }
+
+                    Text(
+                        text = "$value",
+                        fontSize = 30.sp
+                    )
+
+                    Slider(
+                        onValueChangeFinished = {
+                            Log.i("numericSlider_onValue", "finished $value")
+                        },
+                        steps = ((maxValue - minValue)/stepValue).toInt()-1,
+                        valueRange =  minValue..maxValue,
+                        enabled = true,
+                        value = value,
+                        onValueChange = {
+                            value = ((it - minValue)/stepValue).roundToInt()*stepValue + minValue
+                            Log.i("numericSlider_onValue", "$it $value")
+                        },
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .padding(dimensionResource(id = R.dimen.dialog_confirm_button_margin)),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier
+                                .clip(Shapes.small)
+                                .clickable {
+                                    emitValue(value)
+                                    dialogOpen = false
+                                }
+                                .background(colorResource(id = R.color.fieldComponent_button_background))
+                                .padding(dimensionResource(id = R.dimen.dialog_confirm_button_padding))
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.textFieldInput_confirm_text_buttonText),
+                                fontSize = 20.sp
+                            )
+                        }
+                    }
+                }
+            },
+            shape = Shapes.small,
+            backgroundColor = Color.White,
+            properties = DialogProperties(
+                dismissOnBackPress = true,
+                dismissOnClickOutside = true
+            )
+        )
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(dimensionResource(id = R.dimen.fieldComponent_button_padding))
+            .clip(Shapes.small)
+            .background(colorResource(id = R.color.fieldComponent_button_background))
+            .clickable {
+                dialogOpen = true
+            }
+    ) {
+        Text(
+            text = stringResource(id = R.string.numericFieldInput_open_dialog_button),
+            modifier = Modifier
+                .padding(
+                    horizontal = dimensionResource(id = R.dimen.fieldComponent_button_text_padding),
+                    vertical = 0.dp
+                )
+        )
+    }
+
 }
 
 @Preview
@@ -189,9 +287,9 @@ fun PreviewNumericFieldInput() {
         fieldId = 0,
         name = "Field 2 Text",
         minValue = -1f,
-        maxValue = 25f,
-        valueStep = 0.1f,
-        currentValue = 18.1f,
+        maxValue = 3f,
+        valueStep = 1f,
+        currentValue = 2f,
 //        localValue = 18.1f,
     )
     NumericFieldInput(
@@ -199,14 +297,12 @@ fun PreviewNumericFieldInput() {
         emitValue = { value ->
             if (value < viewState.minValue) {
                 viewState.currentValue = viewState.minValue
-//                viewState.localValue = viewState.currentValue
-
             } else if (value > viewState.maxValue) {
                 viewState.currentValue = viewState.maxValue
-//                viewState.localValue = viewState.currentValue
             } else {
-                val Nstep = round((value - viewState.minValue) / viewState.valueStep)
-                viewState.currentValue = viewState.minValue + Nstep * viewState.valueStep
+//                val Nstep = round((value - viewState.minValue) / viewState.valueStep)
+//                viewState.currentValue = viewState.minValue + Nstep * viewState.valueStep
+                viewState.currentValue = value
             }
         },
         modifier = Modifier
